1c884593fb62de3e1218f404dcf3df14c03eedc2
Md Foisal Islam <islammdfoisal54@gmail.com>
2024-10-08 12:25:19 +0600

    some changes in baseApi

--- src/Redux/baseApi/baseApi.ts
+++ src/Redux/baseApi/baseApi.ts
@@ -1,15 +1,16 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { RootState } from '../store/store';
 
 const baseQuery = fetchBaseQuery({
   baseUrl: 'https://techtuend-service-server.vercel.app/api',
   credentials: 'include',
-  // prepareHeaders: (headers, { getState }) => {
-  //   const token = (getState() as RootState).auth.token;
+  prepareHeaders: (headers, { getState }) => {
+    const token = (getState() as RootState).authTechTuend.token;
 
-  //   if (token) {
-  //     headers.set('authorization', token);
-  //   }
-  // },
+    if (token) {
+      headers.set('authorization', token);
+    }
+  },
 });
 export const baseApi = createApi({
   reducerPath: 'baseUrl',

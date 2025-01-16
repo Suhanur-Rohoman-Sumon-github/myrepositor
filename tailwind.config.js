cd999c262b521e23841011359509cd47f0299052
Suhnaur-Rohoman-Sumon-github <sumonsuhanurrohoman@gmail.com>
2025-01-16 15:17:41 +0600

    some more changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        brandPrimary: '#9867FF', // Navy Blue
-        brandSecondary: '#F5F5F5', // Light Gray
-        brandAccent: '#00ADEF', // Bright Cyan
-        brandHighlight: '#FF6A00', // Orange
-        brandText: '#333333', // Dark Gray (for text)
+        brandPrimary: "#9867FF", // Navy Blue
+        brandSecondary: "#F5F5F5", // Light Gray
+        brandAccent: "#00ADEF", // Bright Cyan
+        brandHighlight: "#FF6A00", // Orange
+        brandText: "#333333", // Dark Gray (for text)
       },
       fontFamily: {
-        headingFont: 'Sora',
+        headingFont: "Sora",
       },
     },
   },
-  plugins: [require('daisyui')],
+  plugins: [daisyui],
 };
